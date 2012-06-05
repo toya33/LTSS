df42cfc11b1e1755f51c229b2f06f66aee35a981
user.email <rezel.persona@gmail.com>
2012-06-05 15:57:25 +0900

    add image serach .js

--- google_serach.js
+++ google_serach.js
@@ -10,13 +10,19 @@
 
       // 検索コントロールに画像検索のサーチャーを追加する
       searchControl.addSearcher( new google.search.ImageSearch() );
+	  
+	  searchControl.setResultSetSize('large');
+	  
+	  var options = new GsearcherOptions();
+	  options.setExpandMode(GSearchControl.EXPAND_MODE_OPEN);
+	  searchControl.addSearcher(ws,options);
 
       // 検索コントロールを描画する
-      searchControl.draw( document.getElementById( 'main_img' ) );
-	  console.log("draw method");
+      searchControl.draw( document.getElementById( 'inner_main_img' ) );
 	  
       // 検索を実行する
       searchControl.execute( 'sky' );
+	  
   }
   
   // ドキュメントがロードされた後に呼び出されるハンドラ関数を登録する
---
+++ more_serach02.js
@@ -0,0 +1,58 @@
+﻿  var no   = 0;
+  var imgresults = new Array();
+  
+  function OnLoad()
+  {
+  
+      var imageSearch = new google.search.ImageSearch();
+	
+		imageSearch.setResultSetSize();
+      // 検索完了時に呼び出されるコールバック関数を登録する　　　　　　　　この部分が検索結果？↓　　
+      imageSearch.setSearchCompleteCallback( this, SearchComplete, [ imageSearch ] );
+	  
+      // 検索を実行する
+      imageSearch.execute( 'sky' );
+  }
+
+  function SearchComplete( searcher )
+  {
+      // 結果オブジェクトを取得する
+      var results = searcher.results;
+	  
+      if( results && ( 0 < results.length ) )
+      {
+          // 情報を取得する
+          for( var i = 0; i < results.length; i++ )
+          {	  
+			  imgresults[no++]=[results[i].title,results[i].Url,results[i].tbUrl];  
+			} 
+       }
+	   
+	var current = searcher.cursor;              // cursorオブジェクト
+    var currentPage = current.currentPageIndex;  // 現在のページ番号
+
+    if( currentPage < current.pages.length - 1 )
+    {
+        var nextPage = currentPage + 1;          // 次のページのページ番号
+
+        // 次のページを検索する
+        searcher.gotoPage( nextPage );
+		console.log(nextPage);
+    }
+  }
+
+
+  function drawimg(){
+  
+       var content;
+	   
+	   for(var i = 0 ; i < no ; i++){
+	   content= document.getElementById( 'img'+(i+1) );
+    // サムネイル画像のURL
+       var image = document.createElement( 'img' );
+       image.src = imgresults[i][2];
+	   content.appendChild( image );
+	   }
+  }
+  google.load( 'search', '1' );
+  google.setOnLoadCallback( OnLoad );
